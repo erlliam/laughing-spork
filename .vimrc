a19cdf55fb6b9e781541371cca5ddbc58a62fb8a
Tommejas <tommejas@gmail.com>
2017-10-25 07:18:55 -0400

    slideshow vimrc

---
+++ .vimrc
@@ -0,0 +1,5 @@
+syntax on
+set number
+
+:noremap <Space> :w<CR>
+:noremap <CR> :!python %<CR>
--- slideshow.py
+++ slideshow.py
@@ -1,1 +1,6 @@
+from PIL import Image
+import os
 
+picture = Image.open("/home/mint/Pictures/test/1.jpeg")
+picture.thumbnail((1280, 720))
+picture.show()
