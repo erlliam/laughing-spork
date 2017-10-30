cfd7bb7d83595cbe4bd8c23359a6586d545c2c6d
tommejas <tommejas>
2017-10-30 11:16:47 -0400

    update

--- .vimrc
+++ .vimrc
@@ -1,5 +1,11 @@
 syntax on
+set number
 filetype plugin indent on
+
+
 set tabstop=4
 set shiftwidth=4
 set expandtab
+
+:noremap <Space> :w<CR>
+:noremap <CR> :!python3 %<CR>
--- slideshow/slideshow.py
+++ slideshow/slideshow.py
@@ -1,12 +1,41 @@
-from PIL import Image
-import os, sys, time
+from tkinter import *
+import os
+from PIL import ImageTk, Image
 
 size = (640, 480)
-folder = os.path.abspath("images")
 
-for image in os.listdir(folder):
-	p = Image.open("{}/{}".format(folder, image))
-	p.thumbnail(size)
-	p.show()
-	time.sleep(2)
+folder = "images/"
+test = os.listdir(folder)
 
+class MainGui:
+    def __init__(self, root):
+        self.root = root
+        root.title("Slideshow")
+
+        self.image = Label(root)
+        self.image.pack()
+
+        self.back_button = Button(root, text="<--", command=self.back)
+        self.back_button.pack(side=LEFT)
+
+        self.forward_button = Button(root, text="-->", command=self.forward)
+        self.forward_button.pack(side=RIGHT)
+
+    def back(self):
+        pass
+
+    def forward(self):
+        pass
+
+    def change_picture(self, picture):
+        picture = ImageTk.PhotoImage(Image.open(picture))
+        self.image.config(image=picture)
+        self.image.image = picture
+
+
+root = Tk()
+
+window = MainGui(root)
+print(test[0])
+print(len(test)-1)
+root.mainloop()
