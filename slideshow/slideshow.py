28e101be3ebf5738523193cd4f836248ffbc30f7
tommejas <tommejas>
2017-10-31 11:15:32 -0400

    update

--- slideshow/slideshow.py
+++ slideshow/slideshow.py
@@ -13,14 +13,18 @@ class MainGui:
         self.root = root
         root.title("Slideshow")
 
-        self.image = Label(root)
-        self.image.pack()
-
         self.back_button = Button(root, text="<--", command=self.back)
-        self.back_button.pack(side=LEFT)
+        self.back_button.grid(row=1)
 
         self.forward_button = Button(root, text="-->", command=self.forward)
-        self.forward_button.pack(side=RIGHT)
+        self.forward_button.grid(row=1)
+        
+        self.select_directory = Button(root, text="Select directory", command=self.directory)
+        self.select_directory.grid(row=1) 
+
+        self.image = Label(root)
+        self.image.config()
+        self.image.grid(row=0)
 
     def back(self):
         if self.index - 1 >= 0:
@@ -31,15 +35,18 @@ class MainGui:
             self.index = self.index + 1
             self.change_picture("{}{}".format(folder, files[self.index]))
 
+    def directory(self):
+        pass
+
     def change_picture(self, picture):
         picture = Image.open(picture)
         picture.thumbnail(size)
         picture = ImageTk.PhotoImage(picture)
-        self.image.config(image=picture)
+        self.image.config(image=picture, height=640, width=480)
         self.image.image = picture
 
 
 root = Tk()
-
+#root.resizable(width=False, height=False)
 window = MainGui(root)
 root.mainloop()
